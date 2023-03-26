9d428ece273939315d1dc16759c1506be1dd78cd
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2023-03-26 16:20:20 +0200

    build: remove gql_client

--- src/api/anilist.rs
+++ src/api/anilist.rs
@@ -1,6 +1,16 @@
-use gql_client::{Client};
 use serde::{Deserialize, Serialize};
 
+
+#[derive(Serialize)]
+struct RequestBody<T: Serialize> {
+    query: String,
+    variables: T,
+}
+
+#[derive(Deserialize)]
+struct ResponseBody {
+    data: Data,
+}
 
 #[derive(Deserialize)]
 struct Data {
@@ -35,6 +45,7 @@ pub struct Vars {
 
 pub async fn get_data(id: &i64) -> Result<Media, String> {
     let endpoint = "https://graphql.anilist.co/";
+
     let query = r#"
         query ($id: Int) {
             Media (id: $id, type: ANIME) {
@@ -51,12 +62,24 @@ pub async fn get_data(id: &i64) -> Result<Media, String> {
         }
     "#;
 
-    let client = Client::new(endpoint);
-    let vars = Vars { id: id.clone() };
-    let request = client.query_with_vars_unwrap::<Data, Vars>(query, vars).await;
+    let variables = Vars { id: id.clone() };
 
-    match request {
-        Ok(r) => Ok(r.media),
-        Err(e) => Err(e.message().to_string()),
+    let client = reqwest::Client::new();
+
+    let request_body = RequestBody { query: query.to_string(), variables };
+
+    let resp = 
+        client
+        .post(endpoint)
+        .json(&request_body)
+        .send()
+        .await
+        .unwrap()
+        .json::<ResponseBody>()
+        .await;
+
+    match resp {
+        Ok(response_body) => Ok(response_body.data.media),
+        Err(e) => Err(e.to_string())
     }
 }
